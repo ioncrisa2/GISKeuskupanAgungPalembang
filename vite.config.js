67af11b9718c9b26ddaf5997fbd91fbc97b84d50
ioncrisa2 <zyohanes67@gmail.com>
2024-05-24 08:08:57 +0700

    add primevue ui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
     plugins: [
@@ -17,5 +19,10 @@ export default defineConfig({
                 },
             },
         }),
+        Components({
+            resolvers:[
+                PrimeVueResolver()
+            ]
+        })
     ],
 });
