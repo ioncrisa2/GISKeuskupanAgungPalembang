eb9f98d0da08a1ef5f606b1a436aa6973a6ece38
ioncrisa2 <zyohanes67@gmail.com>
2024-05-24 08:10:14 +0700

    adding routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DekanatController;
+use App\Http\Controllers\Admin\ParokiController;
+use App\Http\Controllers\Admin\StasiController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -18,10 +22,15 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::prefix('admin')->group(function(){
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+    Route::apiResource('dekanat',DekanatController::class)->except('show');
+    Route::apiResource('paroki',ParokiController::class);
+    Route::apiResource('stasi',StasiController::class);
+})->middleware('auth');
 
 require __DIR__.'/auth.php';
