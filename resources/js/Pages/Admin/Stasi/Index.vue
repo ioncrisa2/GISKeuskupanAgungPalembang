40f36214a4348c0e8ee0f03f9ab49e8fa54aa1be
ioncrisa2 <zyohanes67@gmail.com>
2024-05-24 08:10:56 +0700

    initial pages

---
+++ resources/js/Pages/Admin/Stasi/Index.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <Head title="Data dekanat"/>
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Data Dekanat</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Halaman Stasi</div>
+                </div>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from "@inertiajs/vue3";
+
+const props = defineProps({stasi:Object});
+</script>
+<style lang="">
+
+</style>
